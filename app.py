d21a0d10227cbdc901d884915a5c81a3dd22e4be
tomzyhub <tomzy337@gmail.com>
2019-07-27 14:15:44 -0400

    this is a tweet bot that gets a like automatically
     Initial commit
    :wq

---
+++ app.py
@@ -0,0 +1,29 @@
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+class TwitterBot:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.bot = webdriver.Firefox()
+
+    def login(self):
+
+
+        bot = self.bot
+        bot.get('https://twitter.com/')
+        time.sleep(5)
+        email = bot.find_element_by_class_name('email-input')
+        password = bot.find_element_by_class_name('session[password]')
+        email.clear()
+        password.clear()
+        email.send_keys(self.password)
+        password.send_keys(self.password)
+
+        tom = TwitterBot('tomzy337@gmail.com', 'tomzyb')
+        tom.login()
+pass
